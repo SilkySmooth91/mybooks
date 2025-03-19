6c7de9a85f877f17a00807a52e7d1e8c0590a365
SilkySmooth91 <lorenzo.olivieri13@gmail.com>
2025-03-19 18:36:55 +0100

    bordo rosso

--- src/assets/Components/SingleBookComp.jsx
+++ src/assets/Components/SingleBookComp.jsx
@@ -5,12 +5,20 @@ import { Col, Card, } from 'react-bootstrap';
 export default function SingleBookComp({book}) {
 
   const [selected, setSelected] = useState(false);
+  const handleClick = () => {
+    setSelected(!selected)
+  }
+
+  let imgClass = 'book-cover'
+  if (selected) {
+    imgClass += ' selected'
+  }
   
 
   return (
     <Col key={book.asin} md={3} xs={6}>
       <Card className="mb-4">
-        <Card.Img variant="top" src={book.img} />
+        <Card.Img variant="top" src={book.img} onClick={handleClick} className={imgClass}/>
         <Card.Body>
           <Card.Title>{book.title}</Card.Title>
         </Card.Body>
